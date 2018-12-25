879b8aad164e287580552ae23339d3e6eeb34c64
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-12-25 00:25:32 -0600

    .env with express

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+// console.log('running');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
